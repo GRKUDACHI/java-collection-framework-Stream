23947cb23cc0a57691f6263fb6d926369ba599be
Girish_k <girish.k@tya.co.in>
2025-02-17 23:22:01 +0530

    to comparing method Comparator.comparing() method

--- Collection/comparing.java
+++ Collection/comparing.java
@@ -5,14 +5,14 @@ class comparing
     public static void main (String[] args)
     {
         List<Student> stu = new ArrayList<>();
-        
+
         stu.add(new Student("happy",8));
         stu.add(new Student("harry",9));
         stu.add(new Student("raj",6));
         stu.add(new Student("pratik",7));
 
-        Comparator<Student> compare = Comparator.comparing(Student::getScgpa).reversed();
-
+        Comparator<Student> comparator = Comparator.comparing(Student::getScgpa).reversed();
+        stu.sort(comparator);
         for(Student s : stu)
         {
             System.out.println(s.getName()+" : "+s.getScgpa());
