121c8ccd4e85bff480ca8c88bf5913d7b683c9ed
Girish_k <girish.k@tya.co.in>
2025-06-20 10:33:20 +0530

    Two way cretaed list

--- stream/streamUserdefinedobj.java
+++ stream/streamUserdefinedobj.java
@@ -2,20 +2,32 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.*;
+
 
 public class streamUserdefinedobj
 {
     public static  void main(String[] args)
     {
-        List<Employee> emp = Arrays.asList(
-                new Employee(202,"Marketing",20000,"Karnataka","Rakesh"),
-                new Employee(203,"Developer",50000,"Delhi","Happy"),
-                new Employee(292,"Developer",55000,"Karnataka","Girish"),
-                new Employee(202,"Developer",35000,"tamil nadu","Himanshu"),
-                new Employee(202,"Accounts",25000,"mumbai","sid"),
-                new Employee(202,"HR",45000,"Karnataka","ram")
+        // List<Employee> emp = Arrays.asList(
+        //         new Employee(202,"Marketing",20000,"Karnataka","Rakesh"),
+        //         new Employee(203,"Developer",50000,"Delhi","Happy"),
+        //         new Employee(292,"Developer",55000,"Karnataka","Girish"),
+        //         new Employee(202,"Developer",35000,"tamil nadu","Himanshu"),
+        //         new Employee(202,"Accounts",25000,"mumbai","sid"),
+        //         new Employee(202,"HR",45000,"Karnataka","ram")
 
-        );
+        // );
+
+        List<Employee> emp = new ArrayList<>();
+
+        emp.add(new Employee(202,"Marketing",20000,"Karnataka","Rakesh"));
+        emp.add(new Employee(232,"Developer",55000,"Karnataka","Happy"));
+        emp.add(new Employee(22,"Developer",58000,"Karnataka","Harry"));
+        emp.add(new Employee(242,"Developer",60000,"Mumbai","Girish"));
+        emp.add(new Employee(402,"Accounts",25000,"Delhi","Himanshu"));
+        emp.add(new Employee(302,"Digital",45000,"Andhra","sid"));
+        emp.add(new Employee(542,"HR",20000,"Mumbai","guru"));
         
         //Find the employees belong to Karnataka city 
         Predicate<Employee> pred  = em->em.getCity().equals("Karnataka");
